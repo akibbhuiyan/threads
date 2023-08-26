4700765bbdf0a2b1b6af7cfdb9b5b4314e76beaa
akibbhuiyan <akibbhh@gmail.com>
2023-08-26 16:49:56 +0600

    date and Time

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -138,22 +138,29 @@ const ThreadCard = async ({
           </Link>
         </div>
       )}
-      {!isComment && community && (
-        <Link
-          href={`/communities/${community.id}`}
-          className="mt-5 flex items-center"
-        >
-          <p className="text-subtle-medium text-gray-1">
-            {formatDateString(createdAt)} - {community.name} Community
-          </p>{" "}
-          <Image
-            src={community.image}
-            alt={community.name}
-            width={14}
-            height={14}
-          />
-        </Link>
-      )}
+
+      <div className="mt-5 flex items-center">
+        <p className="text-subtle-medium text-gray-1">
+          {formatDateString(createdAt)}
+        </p>{" "}
+        {!isComment && community && (
+          <Link
+            href={`/communities/${community.id}`}
+            className="flex items-center"
+          >
+            <p className="text-subtle-medium text-gray-1">
+              {" "}
+              - {community.name} Community
+            </p>
+            <Image
+              src={community.image}
+              alt={community.name}
+              width={14}
+              height={14}
+            />
+          </Link>
+        )}
+      </div>
     </article>
   );
 };
